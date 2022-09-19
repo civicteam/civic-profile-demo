cd5dc2180dd54cf7076753916390acd90c81d9dc
dankelleher <dankelleher@yahoo.com>
2022-09-19 18:01:34 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, {FormEvent} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {CivicProfile, Profile} from "@civic/profile";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [profile, setProfile] = React.useState<Profile>();
+
+    const loadProfile = (e: FormEvent) => {
+        e.preventDefault()
+        const target = e.target as typeof e.target & { subject: { value: string } };
+        CivicProfile.get(target.subject.value).then(setProfile);
+    }
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <form onSubmit={loadProfile}>
+                    Enter wallet address <input name="subject" type="text" placeholder="Subject"/>
+                    <input type="submit"/>
+                </form>
+                {profile && <div>
+                    <h1>{profile.name?.value}</h1>
+                    <img width={200} src={profile.image?.url} alt="profile"/>
+                </div>}
+            </header>
+        </div>
+    );
 }
 
 export default App;
